b95017b4a57901cc26a9ef9034dd5895ac123e0d
Mathieu Daix <mathieudaixpro@gmail.com>
2020-06-24 09:30:27 +0200

    push pour dev à 2

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,12 @@
 import '../scss/index.scss'
+
+import gsap from 'gsap'
+
+const menu = gsap.timeline({ paused: true, reversed: true, defaults: { duration: 0.6, ease: 'power4.inOut' } })
+
+menu.set(document.body, { overflow: 'hidden' })
+menu.to(document.querySelector('.nav__menu'), { autoAlpha: 1 })
+
+document.querySelector('.nav__icon').addEventListener('click', () => {
+	menu.reversed() ? menu.play() : menu.reverse()
+})
