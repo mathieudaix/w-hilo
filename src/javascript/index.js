df81b12c488b2a1a4fa19cb007cea151ebdf1292
Mathieu Daix <mathieudaixpro@gmail.com>
2020-06-24 11:43:57 +0200

    add media queries

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -5,7 +5,9 @@ import gsap from 'gsap'
 const menu = gsap.timeline({ paused: true, reversed: true, defaults: { duration: 0.6, ease: 'power4.inOut' } })
 
 menu.set(document.body, { overflow: 'hidden' })
-menu.to(document.querySelector('.nav__menu'), { autoAlpha: 1 })
+menu.to(document.querySelector('.nav__icon span:first-of-type'), { translateY: 3.5, rotate: -45 }, 0)
+menu.to(document.querySelector('.nav__icon span:last-of-type'), { translateY: -3.5, rotate: 45 }, 0)
+menu.to(document.querySelector('.nav__menu'), { autoAlpha: 1 }, 0)
 
 document.querySelector('.nav__icon').addEventListener('click', () => {
 	menu.reversed() ? menu.play() : menu.reverse()
